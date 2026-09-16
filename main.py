286caab3bb6b2b163bccf31e076d40ee8be96f71
dipesh267 <dipesh267@hotmail.com>
2026-09-16 09:48:34 -0700

    update commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+import requests
+
+response = requests.get('http://api.open-notify.org/iss-now.json')
+response.raise_for_status()
+response_code = response.status_code
+print(response_code)
+if(response_code == 200):
+    print(response.json()["iss_position"])
+else:
+    print("something broke")
